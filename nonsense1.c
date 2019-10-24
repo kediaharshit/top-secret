98ba65ae434cc322018fa3a44eca49a3027bef79
Harshit Kedia <kediaharshit9@gmail.com>
2019-10-24 23:40:34 +0530

    removed useless stuff

--- nonsense1.c
+++ nonsense1.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
+//just a  test file
 int main(int argc, char const *argv[])
 {
 	printf("Hello, World\n");
-	printf("dikhra hai na?\n");
-	// printf("OCaml ain't the best - kc\n");
 	printf("try this world xD\n");
 	return 0;
 }
