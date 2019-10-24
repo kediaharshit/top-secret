23610b5b644192d4973f3fe7aebd2bd4dd78e1ad
Harshit Kedia <cs17b103@smail.iitm.ac.in>
2019-10-24 23:28:09 +0530

    second test

--- nonsense1.c
+++ nonsense1.c
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[])
 	printf("Hello, World\n");
 	printf("dikhra hai na?\n");
 	// printf("OCaml ain't the best - kc\n");
+	printf("try this world xD\n");
 	return 0;
 }
